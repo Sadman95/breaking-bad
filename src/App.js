6effd70a39a818c72ccb5077cc21f9c3ad49f4d8
Sadman95 <mail.sakib.1995@gmail.com>
2021-09-20 03:18:58 +0600

    breking bad characters

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const headerStyle = {
+    display: "flex",
+    padding: "16px",
+    justifyContent: "center"
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header style={headerStyle}>
+        <img width="10%" src={logo} alt="logo missing" />
+        <h1 style={{marginLeft: "16px", color: "#085B2E"}}>Breaking Bad Characters</h1>
       </header>
+      <BreakingBad></BreakingBad>
     </div>
   );
+}
+
+function BreakingBad(){
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    fetch('https://www.breakingbadapi.com/api/characters')
+    .then(res => res.json())
+    .then(data => setCharacters(data))
+  }, [])
+
+  return(
+    <div className="container">
+      {
+        characters.map(character => <Char img={character.img} name={character.name} nickname={character.nickname}></Char>)
+      }
+    </div>
+  );
+}
+
+function Char(props){
+  const imageStyle = {
+    width: "100%",
+    height: "400px"
+  }
+  const divStyle = {
+    border: "1px solid black",
+    backgroundColor: "lavender"
+  }
+  return(
+    <div className="char" style={divStyle}>
+      <img style={imageStyle} src={props.img} alt="" />
+      <h3>{props.name}</h3>
+      <h4 style={{color: "dimgray"}}>{props.nickname}</h4>
+    </div>
+  )
 }
 
 export default App;
